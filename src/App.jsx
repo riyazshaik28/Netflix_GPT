ea67fc770c638c25a70fcda992e1927b05262614
RIYAZ12697 <sshaikriyaz252@gmail.com>
2025-07-23 22:51:45 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 className="text-3xl font-extrabold bg-amber-300 text-white">ggjaajajsbanm</h1>
+    </>
+  )
+}
+
+export default App
